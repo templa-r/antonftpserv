780682c20566621c585feafe025465231f890b48
sstepler <abess1232@gmail.com>
2026-03-12 02:39:26 +0700

    Update ztest4.py

--- ztest4.py
+++ ztest4.py
@@ -318,8 +318,8 @@ for item in data:
     else:
         diameter_count['unknown'] = diameter_count.get('unknown', 0) + 1
 
-    # Добавление в основной файл (исключая диаметр 15)
-    if diameter != 15, 16:
+    # Добавление в основной файл (исключая диаметры 15 и 16)
+    if diameter not in (15, 16):
         add_product_to_root(root, item, diameter)
 
     # Добавление в дополнительные файлы по диапазонам
